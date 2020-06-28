9b16fc0be9152a996fbbd6aa5600173b57f4c6e2
Dominic Clifton <dominic.clifton@cleanflight.com>
2020-06-28 13:23:55 +0200

    SPRacingF3NEO Custom1

---
+++ src/main/target/SPRACINGF3NEO/SPRACINGF3NEO_CUSTOM1.h
@@ -0,0 +1,8 @@
+#undef USE_BARO_MS5611
+
+#undef USE_SERIALRX_SPEKTRUM
+#undef USE_SERIALRX_CRSF       // Team Black Sheep Crossfire protocol
+#undef USE_SERIALRX_IBUS       // FlySky and Turnigy receivers
+#undef USE_SERIALRX_SUMD       // Graupner Hott protocol
+
+#include "target.h"
